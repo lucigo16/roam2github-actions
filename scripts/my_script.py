3600f9f89cc9a2ba685144a7bf7e7d251a6d824a
wqing2000 <105916381+wqing2000@users.noreply.github.com>
2023-12-02 20:40:20 +0800

    Create my_script.py

---
+++ scripts/my_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+
+roam_graph = os.environ.get("ROAM_GRAPH")
+print("ROAM_GRAPH value:", roam_graph)
+
+if ',' in roam_graph:
+    graph_array = roam_graph.split(',')
+else:
+    graph_array = roam_graph.splitlines()
+
+for v in graph_array:
+    v = v.strip()
+    print("Current value of v is:", v)
+    os.makedirs(f"./markdown/{v}/images", exist_ok=True)
+    os.system(f"python roam_image_backup.py json/{v}.json ./markdown/{v}/images")
+    os.system(f"sed -i 's/- #/\n#/g;s/](</](/g;s/.md>)/.md)/g;s/- \([[:digit:]]*\)\./\n\1\./g;s~(https.*%2F\(.*\)\?alt=media.*)~(./images\/\1)~g' ./markdown/{v}/*.md")
