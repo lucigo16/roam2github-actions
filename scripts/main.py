a5c8ba4d9ab1d453c3575e1d65262b5f6f5f8c75
lucigo16 <wqing2025@outlook.com>
2025-10-26 12:55:06 +0800

    fix: path error

--- scripts/main.py
+++ scripts/main.py
@@ -16,5 +16,5 @@ for v in graph_array:
     v = v.strip()
     print("Current value of v is:", v)
     os.makedirs(f"./markdown/{v}/images", exist_ok=True)
-    os.system(f"python roam_image_backup.py json/{v}.json ./markdown/{v}/images")
+    os.system(f"python scripts/roam_image_backup.py json/{v}.json ./markdown/{v}/images")
     os.system(fr"sed -i 's/- #/\n#/g;s/](</](/g;s/.md>)/.md)/g;s/- \([[:digit:]]*\)\./\n\1\./g;s~(https.*%2F\(.*\)\?alt=media.*)~(./images\/\1)~g' ./markdown/{v}/*.md")
